cb2635b7dc47d07ee6eab491ab2378dacd1614cf
gnu3ra <aballmer42@gmail.com>
2016-05-27 16:13:39 -0500

    Created basic AsyncTasks for transfering files with wifi-p2p

---
+++ app/src/main/java/net/ballmerlabs/scatterbrain/network/wifidirect/MessagingAsyncTask.java
@@ -0,0 +1,12 @@
+package net.ballmerlabs.scatterbrain.network.wifidirect;
+
+/**
+ * Class to send text/sms messages over scatterbrain wifi p2p
+ */
+public class MessagingAsyncTask extends WifiAsyncTask {
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+}
---
+++ app/src/main/java/net/ballmerlabs/scatterbrain/network/wifidirect/WifiAsyncTask.java
@@ -0,0 +1,12 @@
+package net.ballmerlabs.scatterbrain.network.wifidirect;
+
+import android.os.AsyncTask;
+
+/**
+ * Template for AsyncTasks to transfer objects over wifi p2p
+ */
+public abstract class WifiAsyncTask extends AsyncTask {
+
+
+
+}

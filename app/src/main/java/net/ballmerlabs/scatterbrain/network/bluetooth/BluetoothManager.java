adc6bca7d328599ba9bea2376bd38d41a11b11d9
gnu3ra <aballmer42@gmail.com>
2016-07-31 22:54:07 -0500

    Uses bluetooth permission

--- app/src/main/java/net/ballmerlabs/scatterbrain/network/bluetooth/BluetoothManager.java
+++
@@ -1,7 +0,0 @@
-package net.ballmerlabs.scatterbrain.network.bluetooth;
-
-/**
- * Created by user on 7/31/16.
- */
-public class BluetoothManager {
-}
---
+++ app/src/main/java/net/ballmerlabs/scatterbrain/network/bluetooth/ScatterBluetoothManager.java
@@ -0,0 +1,17 @@
+package net.ballmerlabs.scatterbrain.network.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+
+import net.ballmerlabs.scatterbrain.network.wifidirect.ScatterPeerListener;
+
+/**
+ * Abstraction for android bluetooth stack for Scatterbrain.
+ */
+public class ScatterBluetoothManager {
+
+    public BluetoothAdapter adapter;
+
+    public ScatterBluetoothManager() {
+
+    }
+}

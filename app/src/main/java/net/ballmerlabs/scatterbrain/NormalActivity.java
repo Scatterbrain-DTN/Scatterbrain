2fa5ee777d7a1c06501a9a3655dfa75b83e1a784
gnu3ra <aballmer42@gmail.com>
2016-08-09 21:58:00 -0500

    NormalActivity now binds service

--- app/src/main/java/net/ballmerlabs/scatterbrain/NormalActivity.java
+++ app/src/main/java/net/ballmerlabs/scatterbrain/NormalActivity.java
@@ -1,5 +1,10 @@
 package net.ballmerlabs.scatterbrain;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +17,7 @@ import android.widget.TextView;
 import net.ballmerlabs.scatterbrain.network.DeviceProfile;
 import net.ballmerlabs.scatterbrain.network.GlobalNet;
 import net.ballmerlabs.scatterbrain.network.NetTrunk;
+import net.ballmerlabs.scatterbrain.network.ScatterRoutingService;
 
 public class NormalActivity extends AppCompatActivity {
 
@@ -22,6 +28,43 @@ public class NormalActivity extends AppCompatActivity {
     private GlobalNet globnet;
     private DeviceProfile profile;
     private TextView peersView;
+    private ScatterRoutingService service;
+    private boolean scatterBound = false;
+    private ScatterRoutingService mService;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            ScatterRoutingService.ScatterBinder binder =
+                    (ScatterRoutingService.ScatterBinder) service;
+            mService = binder.getService();
+
+            mService.registerOnDeviceConnectedCallback(new Runnable() {
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView senpai_notice = (TextView) findViewById(R.id.notice_text);
+                            senpai_notice.setVisibility(View.VISIBLE);
+                            senpai_notice.setText("Senpai NOTICED YOU! \n and the packet was not corrupt");
+                        }
+                    });
+                }
+            });
+
+            mService.getBluetoothManager().startDiscoverLoopThread(); //TODO: not needed
+            scatterBound = true;
+
+
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            scatterBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +85,10 @@ public class NormalActivity extends AppCompatActivity {
                 updateList();
             }
         });
+
+        Intent bindIntent = new Intent(this, ScatterRoutingService.class);
+        bindService(bindIntent, mConnection, Context.BIND_AUTO_CREATE);
+
 
     }
 

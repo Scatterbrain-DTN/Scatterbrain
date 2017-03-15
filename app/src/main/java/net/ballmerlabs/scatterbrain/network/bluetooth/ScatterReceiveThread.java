3842059ad4a97a58da82c9f4ecaf7468821b10fb
gnu3ra <aballmer42@gmail.com>
2017-03-15 01:14:24 +0000

    added break when too many errors

--- app/src/main/java/net/ballmerlabs/scatterbrain/network/bluetooth/ScatterReceiveThread.java
+++ app/src/main/java/net/ballmerlabs/scatterbrain/network/bluetooth/ScatterReceiveThread.java
@@ -96,6 +96,7 @@ public class ScatterReceiveThread extends Thread{
                         trunk.blman.connectedList.remove(socket.getRemoteDevice().getAddress());
                     }
                     go = false;
+                    break;
                 }
 
             }

1b7a8dbe0552c18c13cbacec2868daaefdb245a4
gnu3ra <gnu3ra@riseup.net>
2017-07-30 04:02:59 -0400

    convert highlevelapi to interface

--- app/src/main/java/net/ballmerlabs/scatterbrain/network/HighLevelAPI.java
+++ app/src/main/java/net/ballmerlabs/scatterbrain/network/HighLevelAPI.java
@@ -1,31 +1,17 @@
 package net.ballmerlabs.scatterbrain.network;
-import net.ballmerlabs.scatterbrain.network.DeviceProfile;
 /**
  * basic interface to the Scatterbrain protocol, to be used by
  * external applications
  */
 @SuppressWarnings({"FieldCanBeLocal", "unused"})
-class HighLevelAPI {
-    @SuppressWarnings("unused")
-    private final DeviceProfile MyProfile;
-
-    @SuppressWarnings("unused")
-    public HighLevelAPI(DeviceProfile profile) {
-        this.MyProfile = profile;
-    }
-
-    @SuppressWarnings("EmptyMethod")
-    public void Advertise() {
-
-    }
-
-    @SuppressWarnings({"EmptyMethod", "UnusedParameters"})
-    public void sendData(DeviceProfile target) {
-
-    }
-
-    @SuppressWarnings({"EmptyMethod", "UnusedParameters"})
-    public void queryServices(DeviceProfile target) {
-
-    }
+interface HighLevelAPI {
+    void startService();
+    void stopService();
+    void Advertise();
+    Object getTransports();
+    void sendDataDirected(DeviceProfile target, byte[] data);
+    void sendDataMulticast(byte[] data);
+    BlockDataPacket[] getTopMessage(int num);
+    int getQueueSize();
+    Object queryServices(DeviceProfile target);
 }

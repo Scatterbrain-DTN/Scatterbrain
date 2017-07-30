d953915be38c6b8f02e3da920570d9168774360b
gnu3ra <gnu3ra@riseup.net>
2017-07-30 04:18:12 -0400

    better highlevelapi

--- app/src/main/java/net/ballmerlabs/scatterbrain/network/HighLevelAPI.java
+++ app/src/main/java/net/ballmerlabs/scatterbrain/network/HighLevelAPI.java
@@ -5,13 +5,33 @@ package net.ballmerlabs.scatterbrain.network;
  */
 @SuppressWarnings({"FieldCanBeLocal", "unused"})
 interface HighLevelAPI {
+
+    //system
     void startService();
     void stopService();
-    void Advertise();
-    Object getTransports();
+    Object getPref();
+    void setPref(Object pref);
+    DeviceProfile getProfile();
+    void setProfile(DeviceProfile prof);
+
+    //peers
+    void scanOn();
+    void scanOff();
+    DeviceProfile[] getPeers();
+    Object queryServices(DeviceProfile target);
+
+    //communications
     void sendDataDirected(DeviceProfile target, byte[] data);
     void sendDataMulticast(byte[] data);
-    BlockDataPacket[] getTopMessage(int num);
+    void postMessagesRecievedHandler(Runnable run); //TODO: make a handler that accepts messages
+    BlockDataPacket[] getTopMessages(int num);
+    BlockDataPacket[] getRandomMessages(int num);
+    BlockDataPacket[] getTopMessages();
+    BlockDataPacket[] getRandomMessages();
+
+    //datastore systems tasks
     int getQueueSize();
-    Object queryServices(DeviceProfile target);
+    void flushDatastore();
+    void setDatastoreLimit(int limit);
+    int getDatastoreLimit();
 }
